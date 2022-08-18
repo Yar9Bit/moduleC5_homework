10d01267db677770fce52b05be3c66c6357d9443
yar9bit <yar9bit@gmail.com>
2022-08-18 14:06:42 +0500

    Final commit

--- task5/main.js
+++ task5/main.js
@@ -1,23 +1,24 @@
 const buttonRequestNode = document.querySelector('button');
 const resultNode = document.querySelector('.res');
+const buttonClearNode = document.querySelector('.btn-clear')
 
 function checkValueNumber() {
     const valueNumber = document.querySelector('.inp1').value;
-    if ((valueNumber >= 1) && (valueNumber <= 10)) {
-        return valueNumber;
+    if ((valueNumber < 1 || valueNumber > 10 || isNaN(valueNumber))) {
+        resultNode.textContent = 'Номер страницы вне диапазона от 1 до 10'
     }
     else {
-        resultNode.textContent = 'Номер страницы вне диапазона от 1 до 10'
+        return valueNumber
     }
 }
 
 function checkLimitNumber() {
     const valueLimit = document.querySelector('.inp2').value;
-    if ((valueLimit >= 1) && (valueLimit <= 10)) {
-        return valueLimit
+    if ((valueLimit < 1 || valueLimit > 10 || isNaN(valueLimit))) {
+        resultNode.textContent = 'Лимит вне диапазона от 1 до 10'
     }
     else {
-        resultNode.textContent = 'Лимит вне диапазона от 1 до 10'
+        return valueLimit
     }
 }
 
@@ -39,18 +40,20 @@ fetch(url)
     `;
             cards = cards + cardBlock;
         });
-
-        resultNode.innerHTML = cards;
+        localStorage.setItem('myStorage', cards)
+        resultNode.innerHTML = cards
     })
     .catch(() => {console.log('error')})
 }
 
-
 buttonRequestNode.addEventListener('click', () => {
-    if (checkValueNumber() && checkLimitNumber()) {
-        fetchRequest(`https://picsum.photos/v2/list?page=${checkValueNumber()}&limit=${checkLimitNumber()}`);
-    }
-    else {
-        resultNode.textContent = 'Номер страницы и лимит вне диапазона от 1 до 10'
+    if (checkValueNumber() && checkLimitNumber()){
+        fetchRequest(`https://picsum.photos/v2/list?page=${checkValueNumber()}&limit=${checkLimitNumber()}`)
     }
 })
+
+buttonClearNode.addEventListener('click', () => {
+    localStorage.clear()
+})
+
+resultNode.innerHTML = localStorage.getItem('myStorage')
